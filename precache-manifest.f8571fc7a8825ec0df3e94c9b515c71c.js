7b10ac9816814f469e2c638fc8e7bbe4a60e00b5
Thai Nguyen <duythai275@gmail.com>
2023-03-07 18:09:52 -0700

    Updates

---
+++ precache-manifest.f8571fc7a8825ec0df3e94c9b515c71c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39578036ebceb0e2d30a1145ae55a151",
+    "url": "/ludycakeshop/index.html"
+  },
+  {
+    "revision": "f123ad9730c2bd5e54e4",
+    "url": "/ludycakeshop/static/css/main.c9d1fc93.chunk.css"
+  },
+  {
+    "revision": "4c8f127591c444c04259",
+    "url": "/ludycakeshop/static/js/2.59810dae.chunk.js"
+  },
+  {
+    "revision": "49a69317dfb6f3fa284f21161a6136a9",
+    "url": "/ludycakeshop/static/js/2.59810dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f123ad9730c2bd5e54e4",
+    "url": "/ludycakeshop/static/js/main.7f1e8910.chunk.js"
+  },
+  {
+    "revision": "a945f1686ade990fb26a",
+    "url": "/ludycakeshop/static/js/runtime-main.dc767c9d.js"
+  },
+  {
+    "revision": "6b056160ce3618aa7e898a5bdff211f2",
+    "url": "/ludycakeshop/static/media/footer.6b056160.jpg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ludycakeshop/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "4d631ecdfd3f02bfe90c232bb08d837c",
+    "url": "/ludycakeshop/static/media/social-instagram.4d631ecd.svg"
+  },
+  {
+    "revision": "b299e67454a56443f6def09d093b5a96",
+    "url": "/ludycakeshop/static/media/social-media-facebook-1.b299e674.svg"
+  }
+]);
